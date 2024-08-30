852563bcb8b4b26b9ff1086bd4ed3f3006dee94a
emeka <chidokaemeka@gmail.com>
2024-08-30 16:16:39 +0100

    Custom Author Authentication

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -9,8 +9,10 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework import viewsets
 from .models import Book
 from .serializers import BookSerializer
+from .permissions import IsAuthorOrReadOnly
+
 
 class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
-    permission_classes = [IsAuthenticated] 
+    permission_classes = [IsAuthenticated, IsAuthorOrReadOnly] 
