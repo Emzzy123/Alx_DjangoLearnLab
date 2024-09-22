dd7becde66949cb1ed5b37714a6dbe8c3e922a0e
emeka <chidokaemeka@gmail.com>
2024-09-22 14:15:31 +0100

    update

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -1,19 +1,23 @@
 from rest_framework import serializers
 from rest_framework.authtoken.models import Token
-from .models import CustomUser
+from django.contrib.auth import get_user_model
+
+# Fetch the custom user model
+User = get_user_model()
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
-        model = CustomUser
+        model = User
         fields = ['username', 'email', 'bio', 'profile_picture', 'followers']
 
 class RegisterSerializer(serializers.ModelSerializer):
     class Meta:
-        model = CustomUser
+        model = User
         fields = ['username', 'email', 'password']
 
     def create(self, validated_data):
-        user = CustomUser.objects.create_user(
+        # Use get_user_model to create a new user
+        user = get_user_model().objects.create_user(
             username=validated_data['username'],
             email=validated_data['email'],
             password=validated_data['password']
@@ -25,3 +29,21 @@ class RegisterSerializer(serializers.ModelSerializer):
 class LoginSerializer(serializers.Serializer):
     username = serializers.CharField()
     password = serializers.CharField(write_only=True)
+
+    def validate(self, data):
+        """
+        Custom validation to authenticate user
+        """
+        username = data.get('username')
+        password = data.get('password')
+
+        if username and password:
+            # Authenticate the user
+            user = User.objects.filter(username=username).first()
+            if not user or not user.check_password(password):
+                raise serializers.ValidationError("Invalid login credentials")
+        else:
+            raise serializers.ValidationError("Must include 'username' and 'password'.")
+
+        data['user'] = user
+        return data
