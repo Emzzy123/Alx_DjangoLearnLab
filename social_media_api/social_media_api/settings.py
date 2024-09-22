cd7e658636d250a15e6fc0989be40d42588eef1a
emeka <chidokaemeka@gmail.com>
2024-09-22 14:40:22 +0100

    Post and comment app

--- social_media_api/social_media_api/settings.py
+++ social_media_api/social_media_api/settings.py
@@ -40,7 +40,13 @@ INSTALLED_APPS = [
     'rest_framework',
     'rest_framework.authtoken',
     'accounts',
+    'posts'
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10
+}
 
 AUTH_USER_MODEL = 'accounts.CustomUser'
 
